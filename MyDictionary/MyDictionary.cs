7a6f9793154197265edf571474663eca4e7714bc
Gokhan <engokhangok@gmail.com>
2022-08-06 16:29:38 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        KeyValuePair<Tkey, Tvalue>[] items;
+
+        public MyDictionary()
+        {
+            items = new KeyValuePair<Tkey, Tvalue>[0];
+
+        } //Default Constructor
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            KeyValuePair<Tkey, Tvalue>[] tempArray = items;  // Geçici dizimizi oluşturduk
+            items = new KeyValuePair<Tkey, Tvalue>[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];  //Geçici dizideki elemanları alıp asıl dizimize geri koyduk.
+            }
+            items[items.Length - 1] = new KeyValuePair<Tkey, Tvalue>(key, value);
+        }
+
+        public void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+        }
+        public int Count
+        {
+            get { return items.Length; }
+        }
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string>cars=new MyDictionary<int,string>();
+            cars.Add(1, "Bmw");
+            cars.Add(2, "Mercedes");
+            cars.Add(3, "Volvo");
+            cars.Add(4, "Citroen");
+
+            Console.WriteLine(cars.Count);
+            Console.WriteLine();
+            cars.ShowList();
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
